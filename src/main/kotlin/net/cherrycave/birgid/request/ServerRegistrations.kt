c0c9b616381ad4396bccd8c5724c8c55d9d7c718
Emma Böcker <stckoverflw@gmail.com>
2023-04-28 15:52:54 +0200

    fix serialization for server registrations

--- src/main/kotlin/net/cherrycave/birgid/request/ServerRegistrations.kt
+++ src/main/kotlin/net/cherrycave/birgid/request/ServerRegistrations.kt
@@ -3,11 +3,13 @@ package net.cherrycave.birgid.request
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.serialization.Serializable
 import net.cherrycave.birgid.GertrudClient
 import net.cherrycave.birgid.command.ServerType
 import net.cherrycave.birgid.utils.Internal
 import net.cherrycave.birgid.utils.implementation
 
+@Serializable
 public data class ServerRegistration(
     val register: Boolean,
     val identifier: String,
@@ -17,7 +19,7 @@ public data class ServerRegistration(
 )
 
 @OptIn(Internal::class)
-public suspend fun GertrudClient.getServerRegistrations(): Result<List<ServerRegistration>> {
+public suspend fun GertrudClient.getServerRegistrations(): Result<Array<ServerRegistration>> {
     val registrations = implementation.httpClient.get(implementation.buildUrl() + "/commands/servers/registrations")
 
     return if (registrations.status.isSuccess()) {
